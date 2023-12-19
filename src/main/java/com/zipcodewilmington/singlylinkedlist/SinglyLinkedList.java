2a34ee13d41e5e1bb3417ce9b5e9572076216618
NathanG741 <nathan.guardado@yahoo.com>
2023-12-18 20:44:42 -0500

    Finished

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,129 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> current;
+    private int size;
+    public SinglyLinkedList(T object) {
+        this.head = new Node<>(object);
+        this.current = head;
+        this.size = 1;
+    }
+    public void add(T object) {
+        Node<T> node = new Node<>(object);
+        current.setNext(node);
+        node.setPrevious(current);
+        this.current = node;
+        this.size++;
+    }
+    public void remove(T object) {
+        Node<T> checkNode = this.head;
+        while (true) {
+            if (checkNode.getObject().equals(object)) {
+                if (checkNode.getPrevious() == null) {
+                    this.head = checkNode.getNext();
+                    this.size--;
+                } else {
+                    checkNode.getPrevious().setNext(checkNode.getNext());
+                    this.size--;
+                }
+                break;
+            } else {
+                if (checkNode.getNext() != null) {
+                    checkNode = checkNode.getNext();
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    public boolean contains(T object) {
+        Node<T> checkNode = this.head;
+        while (true) {
+            if (checkNode.getObject().equals(object)) {
+                return true;
+            } else {
+                if (checkNode.getNext() != null) {
+                    checkNode = checkNode.getNext();
+                } else {
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+    public int find(T object) {
+        Node<T> checkNode = this.head;
+        int index = 0;
+        while (true) {
+            if (checkNode.getObject().equals(object)) {
+                return index;
+            } else {
+                if (checkNode.getNext() != null) {
+                    checkNode = checkNode.getNext();
+                    index++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+    public int size() { return this.size; }
+    public T get(int index) {
+        Node<T> checkNode = this.head;
+        for (int i = 0; i <= index; i++) {
+            if (i == index) {
+                return checkNode.getObject();
+            } else {
+                if (checkNode.getNext() != null) {
+                    checkNode = checkNode.getNext();
+                } else {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> sllNew = new SinglyLinkedList<>(this.head.getObject());
+        for (int i = 1; i < this.size(); i++) {
+            sllNew.add(this.get(i));
+        }
+        return sllNew;
+    }
+    public void sort() {}
+
+    class Node<T> {
+
+        private T object;
+        private Node<T> next;
+        private Node<T> previous;
+
+        public Node(T object) {
+            this.object = object;
+            this.next = null;
+            this.previous = null;
+        }
+
+        public void setPrevious(Node<T> node) {
+            this.previous = node;
+        }
+
+        public Node<T> getPrevious() {
+            return this.previous;
+        }
+        public void setNext(Node<T> node) {
+            this.next = node;
+        }
+
+        public Node<T> getNext() {
+            return this.next;
+        }
+        public T getObject() {
+            return object;
+        }
+
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,103 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void testAdd() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+
+        assertEquals(2, sll.size());
+    }
+
+    @Test
+    public void testRemove() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+        sll.add("test2");
+        sll.add("test3");
+
+        assertEquals(4, sll.size());
+
+        sll.remove("test2");
+        assertEquals(3, sll.size());
+        assertEquals(-1, sll.find("test2"));
+    }
+
+    @Test
+    public void testContains() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        SinglyLinkedList<Integer> sll1 = new SinglyLinkedList<>(1);
+
+        assertTrue(sll.contains("test"));
+        assertFalse(sll.contains("test1"));
+
+        assertTrue(sll1.contains(1));
+        assertFalse(sll1.contains(2));
+    }
+
+    @Test
+    public void testFind() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+        sll.add("test2");
+        sll.add("test3");
+
+        assertEquals(0, sll.find("test"));
+        assertEquals(1, sll.find("test1"));
+        assertEquals(2, sll.find("test2"));
+        assertEquals(3, sll.find("test3"));
+        assertEquals(-1, sll.find("test4"));
+    }
+
+    @Test
+    public void testSize() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+        sll.add("test2");
+        sll.add("test3");
+
+        assertEquals(4, sll.size());
+    }
+
+    @Test
+    public void testGet() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+        sll.add("test2");
+        sll.add("test3");
+
+        assertEquals("test2", sll.get(2));
+        assertNull(sll.get(4));
+    }
+
+    @Test
+    public void testCopy() {
+        SinglyLinkedList<String> sll = new SinglyLinkedList<>("test");
+        sll.add("test1");
+        sll.add("test2");
+        sll.add("test3");
+
+        SinglyLinkedList<String> sllNew = sll.copy();
+
+        for (int i = 0; i < sllNew.size(); i++) {
+            assertEquals(sll.get(i), sllNew.get(i));
+        }
+
+        sllNew.remove("test");
+
+        for (int i = 0; i < sllNew.size(); i++) {
+            assertNotEquals(sll.get(i), sllNew.get(i));
+        }
+    }
+
+    @Test
+    public void testSort() {
+    }
 }
